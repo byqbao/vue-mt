bab8500fa4c02c02febee14f2859be9eb833f61a
byqbao <18101240034@163.com>
2018-08-06 17:32:11 +0800

    启动页

---
+++ src/components/Start.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="start-page">
+		<img class="start-mt" src="../assets/mt_01.jpg" alt="美团">
+		<address class="start-addr">
+			<p>计算机服务</p>
+			<p class="start-addr-com">&copy;metuan.com</p>
+		</address>
+	</div>
+</template>
+
+<script>
+	export default {
+	  	name: 'Start',
+	  	data () {
+	    	return {
+	    	}
+	  	}
+	}
+</script>
+
+<style scoped>
+	
+	.start-mt {
+		width: 37.9%;
+		display: block;
+		margin: 175px auto 0;
+	}
+	.start-addr {
+		position: absolute;
+		bottom: 18px;
+		left: 0;
+		width: 100%;
+		line-height: 14px;
+		text-align: center;
+		font-size: 12px;
+		color: #bcbcbc;
+		font-style: normal;
+	}
+	.start-addr p {
+		margin: 0;
+	}
+	.start-addr-com {
+		color: #cacaca;
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Start from '@/components/Start'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  	routes: [
+	    {
+	      	path: '/',
+	      	name: 'HelloWorld',
+	      	component: HelloWorld
+	    },{
+	    	path: '/start',
+	      	name: 'HelloWorld',
+	      	component: Start
+	    }
+  	]
 })
